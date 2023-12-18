a3b46aa36566791f4d75ac81deb96d9d63e1a204
Daniela Duarte <danielatavaresd@gmail.com>
2023-12-18 22:12:52 +0000

    aula 02 c#

---
+++ aula02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace aula02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CFB cursos");
+            if (args.GetLength(0) > 0)
+            {
+                Console.Write(args.GetValue(0));
+            }
+        }
+    }
+}
