24dc35c7ef2148c8912d9134cedd5fdb8e9982a2
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-10 22:15:39 +0000

    CFB2

--- aula02/Program.cs
+++ aula02/Program.cs
@@ -4,10 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("CFB cursos");
-            if (args.GetLength(0) > 0)
+            Console.WriteLine("CFB cursos"); //C# no padrão .net     Imiprimir CFB Cursos
+           
+            if (args.GetLength(0) > 0) //O if só está sendo usado para previnir algum problema
             {
-                Console.Write(args.GetValue(0));
+                Console.Write(args.GetValue(0)); //Quero obter o índice da posição 0
             }
         }
     }
